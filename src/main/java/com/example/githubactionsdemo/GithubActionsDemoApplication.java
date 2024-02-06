64b01bff99edb4a5f8455c4e7a5e9591903f1bba
Nduris <nduris@hotmail.com>
2024-02-06 10:03:28 +0100

    start

--- src/main/java/com/example/githubactionsdemo/GithubActionsDemoApplication.java
+++ src/main/java/com/example/githubactionsdemo/GithubActionsDemoApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class GithubActionsDemoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(GithubActionsDemoApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(GithubActionsDemoApplication.class, args);
+    }
 
 }
